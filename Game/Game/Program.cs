d7d38846742e96b8befb9c13bd76f250756a8bce
Alexey Vorobev <35743408+w0rm76@users.noreply.github.com>
2026-06-29 23:56:53 +0200

    added homework2

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,89 +1,105 @@
-﻿Console.WriteLine("Hello this is Rock Paper Scissors");
-Console.WriteLine("Enter your step");
+﻿string userInput;
 
-
-var userWon = false;
-
-while (userWon == false)
+// greeting and asking how many rounds
+Console.WriteLine("Hello this is Rock Paper Scissors...and Well");
+Console.WriteLine("How many steps do you want to play? ( 1 to 10 )");
+userInput = Console.ReadLine();
+// parsing the answer
+if (!int.TryParse(userInput, out int rounds) || !(rounds > 0 && rounds <= 10) ) 
 {
-    Console.Clear();
-    
+    Console.WriteLine("Exit the game");
+    return;
+}
+Console.WriteLine($"You chose {rounds} round(s)");
+
+int roundsCounter = 0;
+int userWon = 0;
+int pcWon = 0;
+
+Console.Clear();
+
+while (roundsCounter < rounds)
+{
+    Console.WriteLine("Please, choose your next step:");
+    Console.WriteLine();
     Console.WriteLine("1 - Rock");
     Console.WriteLine("2 - Paper");
     Console.WriteLine("3 - Scissors");
+    Console.WriteLine("4 - Well");
+    Console.WriteLine();
     Console.WriteLine("0 - Exit");
+
+    userInput = Console.ReadLine();
     
-    var userInput = Console.ReadLine(); // "5"
-
+    Console.Clear();
+    
     int userChoice;
-
-    // parse user input = string into int 
-    // put result to out result param
-    // return if parse was successful
-
-    if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 3))
+    
+    if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 4))
     {
         Console.WriteLine("Invalid input");
         continue;
     }
-
     if (userChoice == 0)
     {
         return;
     }
-
-    var random = new Random();
-    var computerChoice = random.Next(1, 4); // generate random number 1-3
-
     string userChoiceString;
     switch (userChoice)
     {
         case 1:
-            Console.WriteLine("Rock");
             userChoiceString = "Rock";
             break;
         case 2:
             userChoiceString = "Paper";
             break;
-        default:
+        case 3:
             userChoiceString = "Scissors";
             break;
+        default:
+            userChoiceString = "Well";
+            break;
     }
-
+    
     Console.WriteLine($"You chose {userChoiceString}");
-
+    
+    var random = new Random();
+    var computerChoice = random.Next(1, 5); // generate random number 1-4
     string computerChoiceString = computerChoice switch
     {
         1 => "Rock",
         2 => "Paper",
-        _ => "Scissors"
+        3 => "Scissors",
+        _ => "Well"
     };
-
     Console.WriteLine($"Computer chose {computerChoiceString}");
-
+    
+    // logic
     if (computerChoice == userChoice)
     {
-        Console.WriteLine("Draw");
+        Console.WriteLine("That's the Draw, try again");
     }
-    else if (userChoice == 1 && computerChoice == 3 || userChoice == 2 && computerChoice == 1 ||
-             userChoice == 3 && computerChoice == 2)
+    else if (userChoice == 1 && computerChoice == 3 || userChoice == 2 && computerChoice == (1 | 4) ||
+             userChoice == 3 && computerChoice == 2 || userChoice == 4 &&  computerChoice == (1 | 3))
     {
-        Console.WriteLine("You win");
-        userWon = true;
+        userWon++;
+        Console.WriteLine("Congrats! You won");
     }
     else
     {
-        Console.WriteLine("You lose");
+        pcWon++;
+        Console.WriteLine("Sorry, you lose");
     }
 
-    // Ctrl + K + D 
+    Console.WriteLine();
+    Console.WriteLine($"Current score is you {userWon} to pc {pcWon}");
+    
+    roundsCounter++;
+    Console.WriteLine($"It were played {roundsCounter} round(s)");
+    
+    if ((pcWon - userWon > rounds - roundsCounter) || (userWon - pcWon > rounds - roundsCounter))
+    {
+        Console.WriteLine($"This is the end");
+        return;
+    }
 }
-
-
-//data in C# 
-//  int, long 5 10 11
-//  float, double, decimal 56.132
-//  chars - 'a'
-//  strings - "text"
-//  bool - true / false
-//  class
